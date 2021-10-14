a758fa3d899b87c0cf159c418b50788b76e1ef51
Andrey Sitnik <andrey@sitnik.ru>
2021-10-14 18:51:31 +0200

    Move Preact integration from Nano Stores

---
+++ index.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'preact/hooks'
+import { listenKeys } from 'nanostores'
+
+export function useStore(store, opts = {}) {
+  let [, forceRender] = useState({})
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (typeof store === 'function') {
+      throw new Error(
+        'Use useStore(Template(id)) or useSync() ' +
+          'from @logux/client/preact for templates'
+      )
+    }
+  }
+
+  useEffect(() => {
+    let batching
+    let rerender = () => {
+      if (!batching) {
+        batching = 1
+        setTimeout(() => {
+          batching = undefined
+          forceRender({})
+        })
+      }
+    }
+    if (opts.keys) {
+      return listenKeys(store, opts.keys, rerender)
+    } else {
+      return store.listen(rerender)
+    }
+  }, [store, '' + opts.keys])
+
+  return store.get()
+}
