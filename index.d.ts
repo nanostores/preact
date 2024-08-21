a4aa6a273966b4b801a58a0c5d818fb67728b2cf
Andrey Petrov <ndt.080@yandex.by>
2024-08-21 21:21:19 +0300

    feat: simplify implementation

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,26 @@
+import type { ReadonlySignal } from '@preact/signals'
 import type { Store, StoreValue } from 'nanostores'
 
-import type { UseStoreOptions } from './core/types'
+export type StoreKeys<T> = T extends {
+  setKey: (k: infer K, v: never) => unknown
+}
+  ? K
+  : never
+
+export interface UseStoreOptions<SomeStore, Value> {
+  keys?: StoreKeys<SomeStore>[]
+  selector?: (state: StoreValue<SomeStore>) => Value
+}
 
 export function useStore<
+  SomeStore extends Store,
+  Value = StoreValue<SomeStore>
+>(
+  store: SomeStore,
+  options: UseStoreOptions<SomeStore, Value> = {}
+): ReadonlySignal<Value>
+
+export function useLegacyStore<
   SomeStore extends Store,
   Value = StoreValue<SomeStore>
 >(store: SomeStore, options: UseStoreOptions<SomeStore, Value> = {}): Value
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+import { useComputed, useSignal } from '@preact/signals'
+import { listenKeys } from 'nanostores'
 import { useSyncExternalStore } from 'preact/compat'
+import { useCallback, useEffect } from 'preact/hooks'
 
-import { defineStore } from './core/defineStore.js'
+function defineStore(store, options = {}, syncExternalStoreFn) {
+  let subscribe = useCallback(
+    onChange => {
+      return options.keys
+        ? listenKeys(store, options.keys, onChange)
+        : store.listen(onChange)
+    },
+    [options.keys, store]
+  )
+
+  let get = useCallback(() => {
+    return (options.selector ?? (s => s))(store.get())
+  }, [options.selector, store])
+
+  return syncExternalStoreFn(subscribe, get)
+}
+
+function useSignalLikeSyncExternalStore(subscribe, getSnapshot) {
+  let cache = useSignal(getSnapshot())
+
+  useEffect(() => {
+    return subscribe(() => (cache.value = getSnapshot()))
+  }, [cache, subscribe, getSnapshot])
+
+  return useComputed(() => cache.value)
+}
 
 export function useStore(store, options = {}) {
+  return defineStore(store, options, useSignalLikeSyncExternalStore)
+}
+
+export function useLegacyStore(store, options = {}) {
   return defineStore(store, options, useSyncExternalStore)
 }
