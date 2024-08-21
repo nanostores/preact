e63e4ae9fce94e8671b2739b281527cead30c223
Andrey Petrov <ndt.080@yandex.by>
2024-08-21 21:31:00 +0300

    feat: update tsdoc comments and file with tests

--- index.d.ts
+++ index.d.ts
@@ -8,10 +8,40 @@ export type StoreKeys<T> = T extends {
   : never
 
 export interface UseStoreOptions<SomeStore, Value> {
+  /**
+   * Will re-render components only on specific key changes.
+   */
   keys?: StoreKeys<SomeStore>[]
+  /**
+   * Allows you to extract value from complex/deep objects
+   */
   selector?: (state: StoreValue<SomeStore>) => Value
 }
 
+/**
+ * Subscribe to store changes and get store’s value.
+ *
+ * Can be user with store builder too.
+ *
+ * ```js
+ * import { useStore } from 'nanostores/preact'
+ *
+ * import { router } from '../store/router'
+ *
+ * export const Layout = () => {
+ *   let page = useStore(router)
+ *   if (page.value.router === 'home') {
+ *     return <HomePage />
+ *   } else {
+ *     return <Error404 />
+ *   }
+ * }
+ * ```
+ *
+ * @param store Store instance.
+ * @param options
+ * @returns Store value.
+ */
 export function useStore<
   SomeStore extends Store,
   Value = StoreValue<SomeStore>
@@ -20,6 +50,30 @@ export function useStore<
   options: UseStoreOptions<SomeStore, Value> = {}
 ): ReadonlySignal<Value>
 
+/**
+ * Subscribe to store changes and get store’s value.
+ *
+ * Can be user with store builder too.
+ *
+ * ```js
+ * import { useLegacyStore } from 'nanostores/preact'
+ *
+ * import { router } from '../store/router'
+ *
+ * export const Layout = () => {
+ *   let page = useLegacyStore(router)
+ *   if (page.router === 'home') {
+ *     return <HomePage />
+ *   } else {
+ *     return <Error404 />
+ *   }
+ * }
+ * ```
+ *
+ * @param store Store instance.
+ * @param options
+ * @returns Store value.
+ */
 export function useLegacyStore<
   SomeStore extends Store,
   Value = StoreValue<SomeStore>
